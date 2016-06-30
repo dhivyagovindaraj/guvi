d02e974649b41cbcbf3606a2c95cc124f1c804a0
dhivyagovindaraj <dhivya16596@gmail.com>
2016-06-30 15:56:23 +0530

    Create allocation3.c

---
+++ allocation3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int l1,l2,l3,n;
+printf("Enter x\n");
+scanf("%d",&l1);
+printf("Enter y\n");
+scanf("%d",&l2);
+printf("Enter z\n");
+scanf("%d",&l3);
+  printf("Enter n\n");
+scanf("%d",&n;
+if(l1&l2)
+{
+printf("2 lab(s) can accomoate 25 students\n");
+  else
+  {
+    printf("None of   lab can accomoate 50 students\n");
+  }
+return 0;
+}
