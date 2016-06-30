a491e8548dae8afb6947712ef81688e18a9477ca
dhivyagovindaraj <dhivya16596@gmail.com>
2016-06-30 16:05:21 +0530

    Create hotel.c

---
+++ hotel.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int mon,days,season=0;
+float rent,normalcost,seasoncost;
+
+{
+  if((mon==11)||(mon=12)||(mon==4)||(mon==6))
+  c=1;
+  switch(c)
+  {
+  case 0:
+  printf("Hotel Tariff: Rs.%.2f",normalcost);
+  break;
+  }
+  case 1:
+    {
+  printf("Hotel Tariff: Rs.%.2f",seasoncost);
+  break;
+    }
+}
+  return 0;
+}
