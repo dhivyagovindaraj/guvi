77eeefc6730a8e3c4230fa773af6bd4cf1561ef8
dhivyagovindaraj <dhivya16596@gmail.com>
2016-06-30 15:07:50 +0530

    Create integers.c

---
+++ integers.c
@@ -0,0 +1,16 @@
+include<stdio.h>
+int main()
+{
+int a,b;
+printf("\nEnter the first number");
+scanf("%d",&a);
+printf("\nEnter the second number");
+scanf("%d",&b);
+if(a<b)
+printf("\n%d is less than %d",a,b);
+else if(a>b)
+printf("\n%d is greater than %d",a,b);
+else
+printf("\n%d is equal to %d",a,b);
+return 0;
+}
