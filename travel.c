e1026703f0ac1eaac4719b001bd0b533cda7edd6
dhivyagovindaraj <dhivya16596@gmail.com>
2016-06-30 15:15:10 +0530

    Create travel.c

---
+++ travel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char  a[50],b[50],c[50];
+printf("\n Enter Details : ");
+printf("Enter Flight Name : ");
+scanf("%s",a);
+printf("\n Enter Starting Location : ");
+scanf("%s",b);
+printf("\n Enter Ending Location : ");
+scanf("%s",c);
+printf("\nTravel Details : ");
+printf("\nThe flight %s starts from %s and reaches %s."a,b,c);
+return 0 ;
+}
